01094068bf1c1e5ea58a85918ffcfbbefe5d1e1a
Ashura <ashuramajestic@gmail.com>
2023-01-26 10:48:57 +0530

    Kehte hai woh credit card abhi tak ghum raha hai

---
+++ js/creditcard.js
@@ -0,0 +1,7 @@
+// function ghum() {
+//     document.getElementById("container").style.transform = "rotateY(180deg)";
+// }
+
+// function ghum2() {
+//     document.getElementById("container").style.transform = "none";
+// }
